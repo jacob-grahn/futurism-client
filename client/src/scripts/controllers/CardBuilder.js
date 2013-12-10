9242bf13d03cf54207aa516ce1507d706c925b05
Jacob Grahn <contact@jiggmin.com>
2013-12-10 00:24:47 -0500

    fixed card deleting

--- client/src/scripts/controllers/CardBuilder.js
+++ client/src/scripts/controllers/CardBuilder.js
@@ -51,7 +51,7 @@ angular.module('futurism')
 
 
 		$scope.deleteCard = function() {
-			CardResource.delete({id:$scope.card.id});
+			CardResource.delete({cardId: $scope.card._id});
 			$scope.applyDefaults();
 		};
 
