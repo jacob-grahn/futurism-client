82877f1561532a37a99abc1e89ef379e96c28abb
Jacob Grahn <contact@jiggmin.com>
2013-12-11 00:38:38 -0500

    added newline and zalgo filter to chat messages

--- client/src/scripts/directives/chatDisplay.js
+++ client/src/scripts/directives/chatDisplay.js
@@ -36,6 +36,18 @@ angular.module('futurism')
 				};
 
 
+				scope.filter = function(str) {
+					//zalgo filter
+					str = str.replace(/[\u0300-\u036f\u0483-\u0489\u1dc0-\u1dff\u20d0-\u20ff\ufe20-\ufe2f]/g, '');
+
+					//newline filter
+					str = str.replace(/\n\r/g, '');
+
+					//all done!
+					return str;
+				};
+
+
 				scope.$watch('chat.getReceivedCount()', function(oldCount, newCount) {
 					if(newCount >= chat.maxMsgs) {
 						var log = elem.find('.chat-log');
