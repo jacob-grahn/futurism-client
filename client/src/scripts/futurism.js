28116b28d67bea9dcbd7efa2d11c32313df4d92a
Jacob Grahn <contact@jiggmin.com>
2013-12-11 23:10:22 -0500

    created instructions page

--- client/src/scripts/futurism.js
+++ client/src/scripts/futurism.js
@@ -4,19 +4,20 @@ angular.module('futurism', ['http-auth-interceptor', 'imageupload', 'ngRoute', '
 
 	.config(function($routeProvider, $locationProvider) {
 		$routeProvider.
-			when('/title', {templateUrl: 'views/title.html', controller: 'TitleCtrl'}).
-			when('/error', {templateUrl: 'views/error.html', controller: 'ErrorCtrl'}).
 			when('/card-builder', {templateUrl: 'views/card-builder.html', controller: 'CardBuilderCtrl'}).
 			when('/card-selector', {templateUrl: 'views/card-selector.html', controller: 'CardSelectorCtrl'}).
+			when('/credits', {templateUrl: 'views/credits.html', controller: 'CreditsCtrl'}).
 			when('/deck-builder', {templateUrl: 'views/deck-builder.html', controller: 'DeckBuilderCtrl'}).
 			when('/deck-selector', {templateUrl: 'views/deck-selector.html', controller: 'DeckSelectorCtrl'}).
-			when('/lobby', {templateUrl: 'views/lobby.html', controller: 'LobbyCtrl'}).
+			when('/error', {templateUrl: 'views/error.html', controller: 'ErrorCtrl'}).
+			when('/futures', {templateUrl: 'views/futures.html', controller: 'FuturesCtrl'}).
 			when('/game/:gameId', {templateUrl: 'views/game.html', controller: 'GameCtrl'}).
 			when('/game-pre-deck/:gameId/:maxPride', {templateUrl: 'views/game-pre-deck.html', controller: 'GamePreDeckCtrl'}).
-			when('/language', {templateUrl: 'views/language.html', controller: 'LanguageCtrl'}).
-			when('/futures', {templateUrl: 'views/futures.html', controller: 'FuturesCtrl'}).
 			when('/guild', {templateUrl: 'views/guild.html', controller: 'GuildCtrl'}).
-			when('/credits', {templateUrl: 'views/credits.html', controller: 'CreditsCtrl'}).
+			when('/instructions', {templateUrl: 'views/instructions.html', controller: 'InstructionsCtrl'}).
+			when('/language', {templateUrl: 'views/language.html', controller: 'LanguageCtrl'}).
+			when('/lobby', {templateUrl: 'views/lobby.html', controller: 'LobbyCtrl'}).
+			when('/title', {templateUrl: 'views/title.html', controller: 'TitleCtrl'}).
 			otherwise({redirectTo: '/title'});
 
 		$locationProvider.html5Mode(true).hashPrefix('#');
