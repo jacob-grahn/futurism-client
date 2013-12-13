bb82c98a51882921cc4a900d3ab5d833f574421c
Jacob Grahn <contact@jiggmin.com>
2013-12-13 03:52:38 -0500

    created player constructor to init some default data

---
+++ server/multi/game/player.js
@@ -0,0 +1,23 @@
+(function() {
+	'use strict';
+
+	/**
+	 * @class Represents a player in a game
+	 * @param {object} account
+	 */
+	var Player = function(account) {
+		var self = this;
+		self._id = account._id;
+		self.teamId = self._id;
+		self.name = account.name;
+		self.site = account.site;
+		self.hand = [];
+		self.graveyard = [];
+		self.cards = [];
+		self.futures = [];
+		self.pride = 0;
+	};
+
+	module.exports = Player;
+
+}());
---
+++ server/spec/multi/game/playerSpec.js
@@ -0,0 +1,17 @@
+describe('game/player', function() {
+
+	var Player = require('../../../multi/game/player');
+
+	it('should set up some defaults', function() {
+		var account = {
+			_id: 1,
+			name: 'bob',
+			site: 'j'
+		};
+
+		var player = new Player(account);
+
+		expect(player._id).toBe(1);
+		expect(player.cards).toEqual([]);
+	});
+});
