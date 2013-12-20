a6599e02e75fb7c63a4a4399e63ebf0bf3df1d22
Jacob Grahn <contact@jiggmin.com>
2013-12-20 15:49:16 -0500

    verified that prizes are awarded after a game finishes

--- server/spec/multi/game/gameSpec.js
+++ server/spec/multi/game/gameSpec.js
@@ -1,129 +1,177 @@
 describe('game', function() {
 	'use strict';
 
+	var mongoose = require('mongoose');
+	var mockgoose = require('mockgoose');
+	mockgoose(mongoose);
+
+	var _ = require('lodash');
 	var Game = require('../../../multi/game/game');
 	var DeckGoose = require('../../../models/deck');
 	var CardGoose = require('../../../models/card');
+	var UserGoose = require('../../../models/user');
 
 
-	it('should play through a simple game', function(done) {
-		DeckGoose.create({
-			_id: 'deck1',
-			name: 'deck1',
-			cards: ['card1'],
-			userId: 1,
-			pride: 17
-		}, function(err) {
-			expect(err).toBe(null);
+	beforeEach(function(done) {
 
-			DeckGoose.create({
-				_id: 'deck2',
-				name: 'deck2',
-				cards: ['card1'],
-				userId: 2,
-				pride: 17
-			}, function(err) {
-				expect(err).toBe(null);
+		UserGoose.create({
+			_id: 1,
+			name: 'phil',
+			site: 'j',
+			group: 'm',
+			elo: 150,
+			fame: 0,
+			fractures: 0
+		},
+		function(err) {
+			if(err) {
+				console.log(err);
+				throw err;
+			}
 
-				CardGoose.create({
-					_id: 'card1',
-					name: 'kicker',
-					pride: 1,
-					abilities: [],
-					attack: 5,
-					health: 2,
-					userId: 123
-				}, function(err) {
-					expect(err).toBe(null);
 
-					var accounts = [
-						{_id:1, name:'phil'},
-						{_id:2, name:'paulina'}
-					];
-					var rules = {};
-					var gameId = 'game1';
+			UserGoose.create({
+				_id: 2,
+				name: 'paulina',
+				site: 'j',
+				group: 'm',
+				elo: 549,
+				fame: 30,
+				fractures: 52
+			},
+			function(err) {
+				if(err) {
+					console.log(err);
+					throw err;
+				}
 
-					var game = new Game(accounts, rules, gameId);
-					expect(game.getStatus().state).toBe('loadup');
 
-					var player1 = game.idToPlayer(1);
-					var player2 = game.idToPlayer(2);
-					expect(player1.name).toBe('phil');
-					expect(player2.name).toBe('paulina');
+				UserGoose.create({
+					_id: 3,
+					name: 'jetson',
+					site: 'j',
+					group: 'm',
+					elo: 13,
+					fame: 9765,
+					fractures: 2
+				},
+				function(err) {
+					if(err) {
+						console.log(err);
+						throw err;
+					}
 
-					game.loadup.selectDeck(player1, 'deck1', function(err) {
-						expect(err).toBe(null);
-						expect(game.getStatus().state).toBe('loadup');
 
-						game.loadup.selectDeck(player2, 'deck2', function(err) {
-							expect(err).toBe(null);
-							expect(game.getStatus().state).toBe('running');
+					DeckGoose.create({
+						_id: 'deck1',
+						name: 'deck1',
+						cards: ['card1'],
+						userId: 1,
+						pride: 17
+					},
+					function(err) {
+						if(err) {
+							console.log(err);
+							throw err;
+						}
 
-							var r1 = game.playCard(player1, player1.hand[0].cid, 0, 0);
-							expect(r1).toBeFalsy();
-							expect(game.board.target(1,0,0).card.name).toBe('phil');
 
-							game.endTurn(player1);
-							game.endTurn(player2);
-							game.endTurn(player1);
-							expect(game.getStatus().state).toBe('removed');
+						DeckGoose.create({
+							_id: 'deck2',
+							name: 'deck2',
+							cards: ['card1'],
+							userId: 2,
+							pride: 17
+						},
+						function(err) {
+							if(err) {
+								console.log(err);
+								throw err;
+							}
 
-							done();
-						})
-					})
+
+							CardGoose.create({
+								_id: 'card1',
+								name: 'kicker',
+								pride: 1,
+								abilities: [],
+								attack: 5,
+								health: 2,
+								userId: 123
+							},
+							function(err) {
+								if(err) {
+									console.log(err);
+									throw err;
+								}
+
+
+								done();
+							});
+						});
+					});
 				});
 			});
 		});
 	});
 
 
-	/*it('should sort players by their deck pride, lowest first', function() {
-		account1.deck.pride = 13;
-		account2.deck.pride = 10;
-		new Game(accounts, rules, gameId);
-		expect(accounts[0]._id).toBe(account2._id);
-		expect(accounts[1]._id).toBe(account1._id);
-
-		account1.deck.pride = 5;
-		account2.deck.pride = 7;
-		new Game(accounts, rules, gameId);
-		expect(accounts[0]._id).toBe(account1._id);
-		expect(accounts[1]._id).toBe(account2._id);
+	afterEach(function() {
+		mockgoose.reset();
 	});
 
 
-	it('gameState should get a complete overview of the game', function() {
-		game = new Game(accounts, rules, gameId);
-		var status = game.getStatus();
-		expect(status.turn).toBe(1);
-		expect(status.accounts[0]).toBe(accounts[0]);
-		expect(status.activeAccountId).toBe(accounts[0]._id);
+	it('should play through a simple game', function(done) {
+		var accounts = [
+			{_id:1, name:'phil'},
+			{_id:2, name:'paulina'}
+		];
+		var rules = {};
+		var gameId = 'game1';
+
+		var game = new Game(accounts, rules, gameId);
+		expect(game.getStatus().state).toBe('loadup');
+
+		var player1 = game.idToPlayer(1);
+		var player2 = game.idToPlayer(2);
+		expect(player1.name).toBe('phil');
+		expect(player2.name).toBe('paulina');
+
+		game.loadup.selectDeck(player1, 'deck1', function(err) {
+			expect(err).toBe(null);
+			expect(game.getStatus().state).toBe('loadup');
+
+			game.loadup.selectDeck(player2, 'deck2', function(err) {
+				expect(err).toBe(null);
+				expect(game.getStatus().state).toBe('running');
+
+				var r1 = game.playCard(player1, player1.hand[0].cid, 0, 0);
+				expect(r1).toBeFalsy();
+				expect(game.board.target(1,0,0).card.name).toBe('phil');
+
+				game.endTurn(player1);
+				game.endTurn(player2);
+				game.endTurn(player1);
+				expect(game.getStatus().state).toBe('awarding');
+
+				_.delay(function() {
+					expect(game.getStatus().state).toBe('removed');
+
+					UserGoose.findById(1, function(err, doc) {
+						expect(err).toBe(null);
+						expect(doc.elo > 150).toBe(true);
+						expect(doc.fame > 0).toBe(true);
+
+						UserGoose.findById(2, function(err, doc) {
+							expect(err).toBe(null);
+							expect(doc.elo < 549).toBe(true);
+							expect(doc.fame > 30).toBe(true);
+
+							done();
+						})
+					});
+				});
+			})
+		})
 	});
-
-
-	it('should do nothing if the wrong player tries to end the turn', function() {
-		game = new Game(accounts, rules, gameId);
-		game.endTurn(account2);
-		expect(game.getStatus().activeAccountId).toBe(account1._id);
-	});
-
-
-	it("should transfer cards from player's decks to their hands", function() {
-		rules.handSize = 3;
-		account1.deck = {
-			cards: [{},{},{},{},{}],
-			pride: 5
-		};
-
-		game = new Game(accounts, rules, gameId);
-		expect(account1.cards.length).toBe(2);
-		expect(account1.hand.length).toBe(3);
-		expect(account2.cards.length).toBe(0);
-		expect(account2.hand.length).toBe(2);
-
-		account1.hand.pop();
-		game.drawCards();
-		expect(account1.cards.length).toBe(1);
-		expect(account1.hand.length).toBe(3);
-	});*/
 });
