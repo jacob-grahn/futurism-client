2ea2bcad9404a8efe006d73b6af2959be22e5d1d
Jacob Grahn <contact@jiggmin.com>
2013-12-26 22:28:56 -0500

    seperated username template out into its own file

--- client/src/scripts/directives/username.js
+++ client/src/scripts/directives/username.js
@@ -11,12 +11,7 @@ angular.module('futurism')
 				site: '@',
 				group: '@'
 			},
-			template: '' +
-				'<a class="username username-{{group}}">' +
-					'<img class="avatar" ng-if="id" ng-src="http://avatars.jiggmin.com/avatars/{{id}}.gif"/>' +
-					'<img class="site-icon" ng-if="site" ng-src="/images/sites/{{site}}.png">' +
-					'<span class="name">{{name}}</span>' +
-				'</a>'
+			templateUrl: 'views/name.html'
 		};
 
 	});
