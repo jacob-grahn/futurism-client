7ce24ab2587a0a1be3c3739d1763ac7067608480
Jacob Grahn <contact@jiggmin.com>
2014-02-02 02:20:45 -0500

    lil' syntax change

--- server/multi/chatInterface.js
+++ server/multi/chatInterface.js
@@ -1,41 +1,38 @@
-(function() {
-	'use strict';
+'use strict';
 
-	var Chat = require('./chat');
-
-
-	var ChatInterface = {
-		initSocket: function(socket) {
+var Chat = require('./chat');
 
 
-			socket.onAccount('chat', function(data, account) {
-				socket.isSilenced(function(err, silenced) {
-					if(err) {
-						return err;
-					}
-					if(silenced) {
-						return socket.emit('silenced', 'you have been silenced');
-					}
-					Chat.safeAdd(account, data.txt, data.roomName);
-				});
+var ChatInterface = {
+	initSocket: function(socket) {
+
+
+		socket.onAccount('chat', function(data, account) {
+			socket.isSilenced(function(err, silenced) {
+				if(err) {
+					return err;
+				}
+				if(silenced) {
+					return socket.emit('silenced', 'you have been silenced');
+				}
+				return Chat.safeAdd(account, data.txt, data.roomName);
 			});
+		});
 
 
-			socket.on('chatHistory', function(roomName) {
-				socket.emit('chatHistory', {
-					roomName: roomName,
-					history: Chat.safeGetHistory(roomName)
-				});
+		socket.on('chatHistory', function(roomName) {
+			socket.emit('chatHistory', {
+				roomName: roomName,
+				history: Chat.safeGetHistory(roomName)
 			});
+		});
 
 
-			socket.on('createChat', function(data) {
-				//Chat.safeCreate(data.roomName);
-			})
-		}
-	};
+		socket.on('createChat', function(data) {
+			//Chat.safeCreate(data.roomName);
+		})
+	}
+};
 
 
-	module.exports = ChatInterface;
-
-}());
+module.exports = ChatInterface;
