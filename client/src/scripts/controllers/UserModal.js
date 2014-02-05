1595704ed70e9a6d56644cf9871bcc39ade6190b
Jacob Grahn <contact@jiggmin.com>
2014-02-05 16:22:13 -0500

    show stats on user modal

--- client/src/scripts/controllers/UserModal.js
+++ client/src/scripts/controllers/UserModal.js
@@ -1,6 +1,7 @@
 angular.module('futurism')
-	.controller('UserModalCtrl', function($scope, userId, UserResource) {
+	.controller('UserModalCtrl', function($scope, userId, UserResource, StatsResource) {
 		'use strict';
 
 		$scope.user = UserResource.get({userId: userId});
+		$scope.stats = StatsResource.get({userId: userId});
 	});
