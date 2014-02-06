6eeea9fe8bc6763abac3f386f6f52a39925f4e71
Jacob Grahn <contact@jiggmin.com>
2014-02-05 23:14:10 -0500

    save message to both the reciever and sender's account

--- client/src/scripts/controllers/UserModal.js
+++ client/src/scripts/controllers/UserModal.js
@@ -1,7 +1,20 @@
 angular.module('futurism')
-	.controller('UserModalCtrl', function($scope, userId, UserResource, StatsResource) {
+	.controller('UserModalCtrl', function($scope, userId, UserResource, StatsResource, $modal) {
 		'use strict';
 
 		$scope.user = UserResource.get({userId: userId});
 		$scope.stats = StatsResource.get({userId: userId});
+
+		$scope.message = function() {
+			$modal.open({
+				templateUrl: 'views/messageModal.html',
+				controller: 'MessageModalCtrl',
+				resolve: {
+					toUserId: function () {
+						return userId;
+					}
+				}
+			});
+			$scope.$dismiss('message');
+		}
 	});
