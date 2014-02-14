1a9101abdce5d16de670a7a657455a49fff30a23
Jacob Grahn <contact@jiggmin.com>
2014-02-14 15:22:15 -0500

    hookups for mods/admins to mod/de-mod and apprentice/de-apprentice

--- client/src/scripts/controllers/UserModal.js
+++ client/src/scripts/controllers/UserModal.js
@@ -1,10 +1,14 @@
 angular.module('futurism')
-	.controller('UserModalCtrl', function($scope, userId, UserResource, StatsResource, $modal) {
+	.controller('UserModalCtrl', function($scope, userId, UserResource, StatsResource, ApprenticeResource, ModeratorResource, $modal) {
 		'use strict';
 
 		$scope.user = UserResource.get({userId: userId});
 		$scope.stats = StatsResource.get({userId: userId});
 
+
+		/**
+		 * Open another modal to send user a message
+		 */
 		$scope.message = function() {
 			$modal.open({
 				templateUrl: 'views/messageModal.html',
@@ -16,5 +20,41 @@ angular.module('futurism')
 				}
 			});
 			$scope.$dismiss('message');
-		}
+		};
+
+
+		/**
+		 * promote user to apprentice
+		 */
+		$scope.apprentice = function() {
+			ApprenticeResource.put({userId: userId});
+			$scope.$dismiss('apprentice');
+		};
+
+
+		/**
+		 * demote an apprentice
+		 */
+		$scope.deApprentice = function() {
+			ApprenticeResource.delete({userId: userId});
+			$scope.$dismiss('deApprentice');
+		};
+
+
+		/**
+		 * promote apprentice to moderator
+		 */
+		$scope.mod = function() {
+			ModeratorResource.put({userId: userId});
+			$scope.$dismiss('mod');
+		};
+
+
+		/**
+		 * demote a moderator
+		 */
+		$scope.deMod = function() {
+			ModeratorResource.delete({userId: userId});
+			$scope.$dismiss('deMod');
+		};
 	});
---
+++ client/src/scripts/services/resources/ApprenticeResource.js
@@ -0,0 +1,10 @@
+angular.module('futurism')
+	.factory('ApprenticeResource', function($resource) {
+		'use strict';
+
+		return $resource('globe/apprentices/:userId', {userId: '@userId'}, {
+			put: {
+				method: 'PUT'
+			}
+		});
+	});
