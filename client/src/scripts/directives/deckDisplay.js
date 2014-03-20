05a8a80d870d93aef3704d69be9eaeebdc9ef409
Jacob Grahn <contact@jiggmin.com>
2014-03-20 15:59:53 -0400

    fix deck preview display

--- client/src/scripts/directives/deckDisplay.js
+++ client/src/scripts/directives/deckDisplay.js
@@ -5,44 +5,29 @@ angular.module('futurism')
 		return {
 			restrict: 'E',
 			replace: true,
-
-			template:
-				'<div class="deck deck-{{size}} deck-{{active}}">' +
-					'<div class="deck-image-holder">' +
-					'<div class="deck-image-1"><img ng-src="{{displayUrls[0]}}"/></div>' +
-					'<div class="deck-image-2"><img ng-src="{{displayUrls[1]}}"/></div>' +
-					'<div class="deck-image-3"><img ng-src="{{displayUrls[2]}}"/></div>' +
-					'</div>' +
-					'<h1 class="deck-name">{{deck.name}}</h1>' +
-					'<h1 class="deck-pride">pride: {{deck.pride}}</h1>' +
-				'</div>',
+			templateUrl: 'views/deck-display.html',
 
 			link: function (scope, elem, attrs) {
 				scope.size = attrs.size || 'large';
 				scope.active = attrs.active || 'active';
+				scope.staticUrl = staticContentUrl.futurism;
+				scope.displayIds = [];
 
 				var deck = scope.deck;
 				var cards = deck.cards;
-				var displayIds = [];
 
+				// give up if there are no cards
 				if(cards.length === 0) {
 					return null;
 				}
 
-				displayIds[0] = cards[0];
-				displayIds[1] = cards[Math.round((cards.length-1)/2)];
-				displayIds[2] = cards[cards.length-1];
+				// pick three cards to show
+				scope.displayIds[0] = cards[0];
+				scope.displayIds[1] = cards[Math.round((cards.length-1)/2)];
+				scope.displayIds[2] = cards[cards.length-1];
 
-				scope.displayUrls = _.map(displayIds, function(card) {
-					var cardId;
-					if(typeof card === 'string') {
-						cardId = card;
-					}
-					else {
-						cardId = card._id;
-					}
-					return staticContentUrl + '/images/cards/' + scope.size + '_' + cardId + '.jpg';
-				});
+				//remove dupes
+				scope.displayIds = _.unique(scope.displayIds);
 			}
 		};
 
