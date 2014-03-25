54e12b14e77be08497e80b514bd7f76d0d61df24
Jacob Grahn <contact@jiggmin.com>
2014-03-25 03:21:26 -0400

    added button to leave guild

--- client/src/scripts/controllers/Guild.js
+++ client/src/scripts/controllers/Guild.js
@@ -1,7 +1,15 @@
 angular.module('futurism')
-	.controller('GuildCtrl', function($scope, $routeParams, GuildResource) {
+	.controller('GuildCtrl', function($scope, $routeParams, $location, GuildResource, MemberResource, me) {
 		'use strict';
 
 		$scope.guildId = $routeParams.guildId;
 		$scope.guild = GuildResource.get({guildId: $routeParams.guildId});
+
+
+		$scope.leaveGuild = function() {
+			MemberResource.delete({guildId: $routeParams.guildId, userId: me.user._id});
+			$location.url('/guild-joiner');
+		};
+
+
 	});
