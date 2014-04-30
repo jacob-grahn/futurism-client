710ab24b8b206fa88e7cca972a213e3b40514c83
Jacob Grahn <contact@jiggmin.com>
2014-04-29 20:10:47 -0400

    got the store roughly working

--- src/scripts/controllers/Store.js
+++ src/scripts/controllers/Store.js
@@ -1,9 +1,25 @@
 angular.module('futurism')
-    .controller('StoreCtrl', function($scope, shared, moustache) {
+    .controller('StoreCtrl', function($scope, shared, me, FutureResource) {
         'use strict';
         
         $scope.futures = shared.futures;
-        $scope.fractures = 7;
-        $scope.fracturesNeeded = 4;
-        $scope.moustache = moustache;
+        $scope.me = me;
+        
+        $scope.iHaveFuture = function(futureId) {
+            if(me.stats && me.stats.futures) {
+                return me.stats.futures.indexOf(futureId) !== -1;
+            }
+            else {
+                return false;
+            }
+        };
+        
+        $scope.buyFuture = function(futureId) {
+            if($scope.iHaveFuture(futureId)) {
+                return false;
+            }
+            FutureResource.put({userId: me.user._id, futureId: futureId}, function() {
+                me.reload();
+            });
+        };
     });
---
+++ src/scripts/services/resources/FutureResource.js
@@ -0,0 +1,10 @@
+angular.module('futurism')
+    .factory('FutureResource', function($resource) {
+        'use strict';
+
+        return $resource('api/users/:userId/futures/:futureId', {userId: '@userId', futureId: '@futureId'}, {
+            put: {
+                method: 'PUT'
+            }
+        });
+    });
