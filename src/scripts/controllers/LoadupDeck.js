ee016fa2da02ee8e33e57f4974784533dacbbceb
Jacob Grahn <contact@jiggmin.com>
2014-05-02 05:41:42 -0400

    load canon decks as defaults before a match

--- src/scripts/controllers/LoadupDeck.js
+++ src/scripts/controllers/LoadupDeck.js
@@ -4,7 +4,7 @@ angular.module('futurism')
 
         loadup.resumePrep();
 
-        $scope.decks = DeckResource.query(function(){});
+        $scope.decks = DeckResource.query({canon: true}, function(){});
         $scope.rules = loadup.rules;
 
         $scope.select = function(deck) {
