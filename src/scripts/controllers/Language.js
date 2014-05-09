07cea003a78bcf067b8dedc57134fb21dcc33573
Jacob Grahn <contact@jiggmin.com>
2014-05-08 21:42:17 -0400

    update language page to load data directly instead of expecting it to be a part of the language phrases

--- src/scripts/controllers/Language.js
+++ src/scripts/controllers/Language.js
@@ -1,4 +1,10 @@
 angular.module('futurism')
-    .controller('LanguageCtrl', function() {
+    .controller('LanguageCtrl', function($scope, $http) {
         'use strict';
+        
+        $scope.languages = {};
+            
+        $http.get('data/languages.json').success(function(res) {
+            $scope.languages = res;
+        });
     });
